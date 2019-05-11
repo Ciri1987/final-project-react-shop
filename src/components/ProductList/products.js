95b59f3710a6469fbfb5fec4feafa4faa44f9370
Anna Strzała <strzala.ania@gmail.com>
2019-05-11 14:50:36 +0200

    Create list of products in the shop

---
+++ src/components/ProductList/products.js
@@ -0,0 +1,110 @@
+export default
+    {
+        "products": [
+            {
+                "name": "Brow Palette",
+                "price": "50.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": "Sale",
+                "imgSrc": "../../images/brow-palette.jpeg"
+            },
+            {
+                "name": "Brush",
+                "price": "30.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": "Bestseller",
+                "imgSrc": "../../images/brush.jpeg"
+            },
+            {
+                "name": "Cream",
+                "price": "60.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": " ",
+                "imgSrc": "../../images/cream.jpeg"
+            },
+            {
+                "name": "Eyeshadow Palette",
+                "price": "45.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": "Sale",
+                "imgSrc": "../../images/eyeshadow-palette.jpeg"
+            },
+            {
+                "name": "Eyeshadow Palette",
+                "price": "65.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": "New",
+                "imgSrc": "../../images/eyeshadow-palette-2.jpeg"
+            },
+            {
+                "name": "Gel",
+                "price": "20.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": " ",
+                "imgSrc": "../../images/gel.jpeg"
+            },
+            {
+                "name": "Green Nail Polish",
+                "price": "10.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": "Sale",
+                "imgSrc": "../../images/green-nail-polish.jpeg"
+            },
+            {
+                "name": "Lipstick",
+                "price": "12.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": "Bestseller",
+                "imgSrc": "../../images/lipstick.jpeg"
+            },
+            {
+                "name": "Lipsticks set",
+                "price": "40.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": "Sale",
+                "imgSrc": "../../images/lipsticks.jpeg"
+            },
+            {
+                "name": "Nail polish",
+                "price": "5.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": "Sale",
+                "imgSrc": "../../images/nail-polish.jpeg"
+            },
+            {
+                "name": "Pink Blush",
+                "price": "24.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": " ",
+                "imgSrc": "../../images/pink-blush.jpeg"
+            },
+            {
+                "name": "Powder",
+                "price": "30.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": "Sale",
+                "imgSrc": "../../images/puder.jpeg"
+            },
+            {
+                "name": "Red lipstick",
+                "price": "5.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": "Last one",
+                "imgSrc": "../../images/red-lipstic.jpeg"
+            },
+            {
+                "name": "Shadow",
+                "price": "20.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": " ",
+                "imgSrc": "../../images/shadow.jpeg"
+            },
+            {
+                "name": "Sponge set",
+                "price": "10.00 $",
+                "descr": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "addInfo": "Sale",
+                "imgSrc": "../../images/sponge.jpeg"
+            },
+        ],
+    }
