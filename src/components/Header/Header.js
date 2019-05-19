9b82fe8da44f1f47721befda4f134fdcf410eacc
Anna Strzała <strzala.ania@gmail.com>
2019-05-19 23:40:12 +0200

    Some changes to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,30 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-
-//style import
-import "./Header.scss";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.scss';
 
 export const Header = props => {
     return (
-        <nav className="main-nav">
-            <div className="shop-name"> Beauty Shop
-        </div>
+        <div className="shop">
+            <div className="shop-name">
+                BEAUTY SHOP
+            </div>
             <div className="header-links">
-                <NavLink exact to="/home" activeClassName="active">
+                <NavLink exact to="/home" activeClassName="activeLink">
                     Home
                 </NavLink>
-                <NavLink exact to="/faq" activeClassName="active">
+                <NavLink exact to="/faq" activeClassName="activeLink">
                     FAQ
                 </NavLink>
-                <NavLink exact to="/terms" activeClassName="active">
+                <NavLink exact to="/terms" activeClassName="activeLink">
                     Terms
                 </NavLink>
-                <NavLink exact to="/contact" activeClassName="active">
+                <NavLink exact to="/contact" activeClassName="activeLink">
                     Contact
                 </NavLink>
-                <NavLink exact to="/cart" activeClassName="active">
-                    Cart
+                <NavLink exact to="/basket" activeClassName="activeLink">
+                    Basket
                 </NavLink>
             </div>
-        </nav>
-
-    );
-};
+        </div>
+    )
+}
