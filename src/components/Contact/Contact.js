e2ee01979831a41b378f98eb4564d9b528bd8d88
Anna Strzała <strzala.ania@gmail.com>
2019-05-24 13:22:45 +0200

    Add social media buttons & import icons

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,16 +1,22 @@
 import React from 'react';
-
-//import styles
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Contact.scss';
 
 export const Contact = () => {
     return (
         <div className="contact">
-            <h2 className="adress">Downing Street 302901, New York, United States of America
+            <h1 className="contact-header"> Please, feel free to contact us!</h1>
+            <h2 className="contact-adress">Downing Street 302901, New York, United States of America
             </h2>
-            <h3 className="phone-number">
-                +500 000 544 554
+            <h3 className="contact-phone-number">
+                +500 000 544
             </h3>
+            <div className="contact-social">
+                <div className="contact-social-twitter"><FontAwesomeIcon icon={['fab', 'twitter']} size="lg" /></div>
+                <div className="contact-social-facebook"> <FontAwesomeIcon icon={['fab', 'facebook']} size="lg" /></div>
+                <div className="contact-social-instagram"> <FontAwesomeIcon icon={['fab', 'instagram']} size="lg" /></div>
+                <div className="contact-social-github">  <FontAwesomeIcon icon={['fab', 'github']} size="lg" /></div>
+            </div>
         </div>
     )
 }
