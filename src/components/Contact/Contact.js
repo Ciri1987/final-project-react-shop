b2bf79884ef4e85628f4ae55e4133b52f1bf614c
Anna Strzała <strzala.ania@gmail.com>
2019-05-25 14:53:03 +0200

    Add mail & phone

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,9 +8,10 @@ export const Contact = () => {
             <h1 className="contact-header"> Please, feel free to contact us!</h1>
             <h2 className="contact-adress">Downing Street 302901, New York, United States of America
             </h2>
-            <h3 className="contact-phone-number">
-                +500 000 544
-            </h3>
+
+            <a className="contact-phone" href="tel:+00822132732">+00822132732</a>
+            <a className="contact-mail" href="mailto:beauty@shop.com">beauty@shop.com</a>
+            <h3 className="contact-title">You can find us on:</h3>
             <div className="contact-social">
                 <div className="contact-social-twitter"><FontAwesomeIcon icon={['fab', 'twitter']} size="lg" /></div>
                 <div className="contact-social-facebook"> <FontAwesomeIcon icon={['fab', 'facebook']} size="lg" /></div>
